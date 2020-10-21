460c411f110468267a62b97bd4c2b1301782bf1e
oskar5331 <oskar.oja@gmail.com>
2020-10-21 17:57:27 +0300

    small change deploy file

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -3,7 +3,7 @@ const execa = require("execa");
 
   (async () => {
     try {
-    //   await execa("git", ["checkout", "--orphan", "gh-pages"]);
+      await execa("git", ["checkout", "--orphan", "gh-pages"]);
       await execa("git", ["checkout", "gh-pages"]);
       console.log("Building...");
       await execa("npm", ["run", "build"]);
@@ -13,9 +13,9 @@ const execa = require("execa");
       await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
       console.log("Pushing to gh-pages...");
       await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    //   await execa("rm", ["-r", folderName]);
+      await execa("rm", ["-r", folderName]);
       await execa("git", ["checkout", "-f", "main"]);
-    //   await execa("git", ["branch", "-D", "gh-pages"]);
+      await execa("git", ["branch", "-D", "gh-pages"]);
       console.log("Successfully deployed");
     } catch (e) {
       console.log(e.message);
