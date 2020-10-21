afe9092675b2244f30d9959134a60d617f1cc0e1
oskar5331 <oskar.oja@gmail.com>
2020-10-21 07:56:00 +0300

    First commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,26 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+import Locatanator from '../views/Locatanator.vue'
 
 const routes: Array<RouteRecordRaw> = [
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '',
+    name: 'Locatanator',
+    component: Locatanator
   }
+  // {
+  //   path: 'about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = createRouter({
---
+++ src/views/Locatanator.vue
@@ -0,0 +1,87 @@
+<template>
+    
+</template>
+<script lang="ts" src="../Utl/Locatanator.js"></script>
+
+<style lang="scss">
+.container{
+  width: 50rem;
+  height: 100%;
+  margin: 0 auto;
+  background-color: #fdfbfb;
+  padding: 2rem 1rem;
+}
+.dropbox {
+    outline: 2px dashed grey; /* the dash box */
+    outline-offset: -10px;
+    background: lightcyan;
+    color: dimgray;
+    padding: 10px 10px;
+    height: 20rem; /* minimum height */
+    position: relative;
+    cursor: pointer;
+    margin: 2rem auto;
+  }
+  .readyButton:hover{
+    background: #4191a1;
+  }
+  .bottom {
+    display: flex;
+    justify-content: space-between;
+  }
+  .input-file {
+    opacity: 0; /* invisible but it's there! */
+    width: 100%;
+    height: 20rem;
+    position: absolute;
+    cursor: pointer;
+  }
+  .input-text {
+    font-size: 1.2rem;
+    margin: 1rem 0rem;
+  }
+
+  .dropbox:hover {
+    background: lightblue; /* when mouse over to the drop zone, change color */
+  }
+
+  .dropbox p {
+    text-align: center;
+    padding: 50px 0;
+  }
+  .readyButton{
+    display: inline-block;
+    background: #4E9CAF;
+    padding: 1rem;
+    text-align: center;
+    border-radius: 0.5rem;
+    color: white;
+    font-weight: bold;
+    line-height: 1.5rem;
+    margin: 1rem;
+    cursor: pointer;
+  }
+  a.ints{
+    cursor: pointer;
+  }
+
+  .fade-enter-active { transition-duration: 2s; }
+  .fade-leave-active { transition-duration: 6s; }
+  .slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(50px);
+  opacity: 0;
+}
+.error {
+    background: #fdfbfb;
+    // background: #ff7301;
+    outline: 2px dashed  #f02d13
+}
+p.error_text {
+  color: #f02d13;
+}
+.error:hover {
+  background: #e6e7e6;
+  opacity: 75%;
+}
+</style>
